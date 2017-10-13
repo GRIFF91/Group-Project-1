8751f0337e1e27f4988a4d707082a78539d62627
Bo Slott <30351649+boslott@users.noreply.github.com>
2017-10-13 14:02:01 -0400

    previous launch button and next launch button working, map displays correctly

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,16 @@
 console.log("test");
 
+var launchName = document.getElementById("launchName");
+var missionDescription = document.getElementById("missionDescription");
+var launchLocation = document.getElementById("launchLocation");
+var agencyName = document.getElementById("agencyName");
+var launchTime = document.getElementById("launchTime");
+var launchPrev = document.getElementById("previousLaunch");
+var launchNext = document.getElementById("nextLaunch");
+var queryURL = "https://launchlibrary.net/1.2/launch/next/20";
+var launchNum = 0;
+
+
 // Create the parallax function.
 function parallax(){
   var parallax_layer1 = document.getElementById('parallax_layer1');
@@ -18,6 +29,7 @@ var geocoder;
 var map;
 function initialize() {
   geocoder = new google.maps.Geocoder();
+  // var latlng = new google.maps.LatLng(28.396837, ‎-80.605659);
   var latlng = new google.maps.LatLng(-34.397, 150.644);
   var mapOptions = {
     zoom: 8,
@@ -26,9 +38,9 @@ function initialize() {
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 
-function codeAddress() {
-  var address = document.getElementById("address").value;
-  geocoder.geocode( {"address": address}, function(results, status) {
+function codeAddress(location) {
+  var address = location;
+  geocoder.geocode( {"address": location}, function(results, status) {
     if (status == "OK") {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
@@ -41,7 +53,31 @@ function codeAddress() {
   });
 }
 
-var queryURL = "https://launchlibrary.net/1.2/launch/next/5";
+function displayLaunchInfo(num, response) {
+  // var launchName = document.getElementById("launchName");
+  // var missionDescription = document.getElementById("missionDescription");
+  // var launchLocation = document.getElementById("launchLocation");
+  // var agencyName = document.getElementById("agencyName");
+  // var launchTime = document.getElementById("launchTime");
+
+  launchName.textContent = response.launches[num].name;
+  missionDescription.textContent = response.launches[num].missions[0].description;
+  launchLocation.textContent = response.launches[num].location.name;
+  agencyName.textContent = response.launches[num].rocket.agencies[0].name;
+  launchTime.textContent = response.launches[num].net;
+
+  // displayLocation();
+  initialize();
+  // setTimeout(codeAddress, 200, response.launches[num].location.name);
+  codeAddress(response.launches[num].location.name);
+}
+
+// function displayLocation() {
+//   initialize();
+//
+// }
+
+
 
 
 $.ajax({
@@ -50,8 +86,13 @@ $.ajax({
         })
 
         .done(function(response) {
+          console.log(response);
+          displayLaunchInfo(launchNum, response);
+          previousLaunch(response);
+          nextLaunch(response);
+
           console.log(queryURL);
-          console.log(response)
+          console.log(response);
           // mission name
           console.log(response.launches[0].name);
 
@@ -71,3 +112,26 @@ $.ajax({
           console.log(response.launches[0].net);
 
         });
+
+        // <li id="changeInfoBtns">
+        //   <button type="button" id="previousLaunch" class="btn btn-default">Previous Launch</button>
+        //   <button type="button" id="nextLaunch" class="btn btn-default">Next Launch</button>
+        // </li>
+
+function previousLaunch(response) {
+  launchPrev.addEventListener("click", function() {
+    if (launchNum > 0) {
+      launchNum--;
+      displayLaunchInfo(launchNum, response);
+    }
+  });
+}
+
+function nextLaunch(response) {
+  launchNext.addEventListener("click", function() {
+    if(launchNum < 20) {
+      launchNum++;
+      displayLaunchInfo(launchNum, response);
+    }
+  });
+}
